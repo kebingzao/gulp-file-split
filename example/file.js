b4cc04e3a36b7c79d7e8e0ae96ca9b27fc8fdac4
Michael Ternrozky <Michael@Sensory.local>
2016-07-03 09:51:24 +0300

    Added a split by pattern instead of count

---
+++ example/file.js
@@ -0,0 +1,6 @@
+/*--FILE WIND --*/
+1234567890 abcde
+/*--FILE FIRE --*/
+9655479879874 reat
+/*--FILE ALUMINIUM --*/
+135135135 tast
--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,19 +1,20 @@
-var gulp  = require('gulp');
-var gsplit  = require('../index');
-var del    = require('del');
+var gulp = require('gulp');
+var gsplit = require('../index');
+var del = require('del');
 
 gulp.task('clean', function(cb) {
     del(['tmp'], cb);
 });
 
 gulp.task('compress', ['clean'], function() {
-  gulp.src('file.txt')
-          .pipe(gsplit({
-              prefix: 'resource_',
-              ext: 'txt',
-              count: 3
-          }))
-          .pipe(gulp.dest('tmp'));
+    gulp.src('file.js')
+        .pipe(gsplit({
+            prefix: 'script_',
+            ext: 'js',
+            //count: 3,
+            splitPattern: "/*--FILE"
+        }))
+        .pipe(gulp.dest('tmp'));
 });
 
 gulp.task('default', ['compress']);
